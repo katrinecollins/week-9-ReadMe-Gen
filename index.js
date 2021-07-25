8e80f4c8ee5dfe6f4803332e6f15750c3c3a17c7
Katrine Collins <katrine.collins1@gmail.com>
2021-07-25 12:25:39 -0400

    files added

--- index.js
+++ index.js
@@ -1,92 +1,116 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
+const fs = require('fs')
+
 const generateMarkdown = require('./utils/generateMarkdown.js');
-const generatePage = (obj)=>{
+
+
+const generatePage = (obj) => {
     return `
-<head>
-    #${obj.title}
-</head>
-<body>
-  #${obj.name}
-  #[title](https://github.com/${obj.github})
-  #Description
-  #${obj.ReadMeDescription}
-  #Table of Contents
-  #${obj.TableofContents}
-  #Installation
-  #${obj.Installation}
-  #Usage
-  #${obj.Usage}
-  #License
-  #${obj.License}
-  #Contributing
-  #${obj.Contributing}
-  #Tests
-  #${obj.Tests}
-  #Questions
-  #${obj.Questions}
-</body>
-</html>
+# ${obj.title}
+
+${obj.name}
+
+[https://github.com/${obj.github}](https://github.com/${obj.github})
+
+## Description
+${obj.ReadMeDescription}
+
+## Installation
+${obj.Installation}
+
+## Usage
+${obj.Usage}
+
+## License
+${obj.License}
+
 `;
+
+// #Contributing
+// #${obj.Contributing}
+// #Tests
+// #${obj.Tests}
+// #Questions
+// #${obj.Questions}
 };
 
 // TODO: Create an array of questions for user input
-const questions = ()=> {
-    return inquirer.prompt([
-        {
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?",
-    validate: titleInput => {if(titleInput){
-        return true
-    } else(console.log("Please enter your project title"))
-return false;
-}
-}, 
+const questions = [
     {
         type: "input",
-        name: "description",
-        message: "What is the description of your project?",
-    }, 
-    {    
-    type: "input",
-    name: "your name",
-    message: "What is your name?",
+        name: "title",
+        message: "What is the title of your project?",
+        validate: titleInput => {
+            if (titleInput) {
+                return true
+            } else (console.log("Please enter your project title"))
+            return false;
+        }
     },
     {
-        type:"input",
-        name:"github id",
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+    },
+    {
+        type: "input",
+        name: "github",
         message: "What is your github id?"
     },
-    
-   {
-    type: "checkbox",
-    name: "license",
-    message: "Which is your license?",
-    choices: [
-        "MIT",
-        "Apache",
-        "GNU",
-        "Bash"],
-   },
-])};
-    /* Pass your questions in here */
-    
+    {
+        type: "input",
+        name: "ReadMeDescription",
+        message: "What is the description of your project?",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "How to install",
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "How to use",
+    },
+    {
+        type: "list",
+        name: "License",
+        message: "Which is your license?",
+        choices: [
+            "MIT",
+            "Apache",
+            "GNU",
+            "Bash"],
+    },
+];
+/* Pass your questions in here */
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+ }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-    .prompt(questions)
-    .then((answers) => {
-        // Use user feedback for... whatever!!
-       
-        const myREADME = generateMarkdown(answers);
+        .prompt(questions)
+        .then((answers) => {
+            // Use user feedback for... whatever!!
+            console.log(answers);
 
-        console.log(myREADME)
-    })
+            const myREADME = generatePage(answers);
+
+            console.log(myREADME);
+
+           
+            fs.writeFile('./output/README.md', myREADME, () => {
+                console.log('README creation success!')
+            })
+
+
+        })
 }
 
 // Function call to initialize app
---
+++ output/README.md
@@ -0,0 +1,19 @@
+
+# Title
+
+dewscription
+
+[https://github.com/adsas](https://github.com/adsas)
+
+## Description
+asdsa
+
+## Installation
+asd
+
+## Usage
+asd
+
+## License
+MIT
+
